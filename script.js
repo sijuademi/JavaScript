a3d565ed612d03b7be938e1dbf33f771ace7b7a7
Ibrahim Adebayo <sijuadebayo@gmail.com>
2022-02-16 21:53:04 +0100

    second commit

--- script.js
+++ script.js
@@ -550,7 +550,88 @@ for (var i = ibrahim.length -1; i >= 0; i--) {
 }
 
 
+// Coding challenge
 
+var ibrahim = {
+    fullName: 'Ibrahim Opeyemi',
+    bills: [124, 48, 268, 180, 42],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++)
+        {
+            // Determine percentage based on tipping rules
+            var percentage;
+            var bill = this.bills[i];
+
+            if (bill < 50){
+                percentage = .2;
+            } else if (bill >= 50 && bill < 200) {
+                percentage = .15;
+            } else {
+                percentage = .1;
+            }
+
+            // Add results to the corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+}
+
+
+
+var khalil = {
+    fullName: 'Khalil Opeyemi',
+    bills: [77, 475,  110, 45],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+        
+        for (var i = 0; i < this.bills.length; i++)
+        {
+            // Determine percentage based on tipping rules
+            var percentage;
+            var bill = this.bills[i];
+            
+            if (bill < 100){
+                percentage = .2;
+            } else if (bill >= 100 && bill < 300) {
+                percentage = .15;
+            } else {
+                percentage = .25;
+            }
+            
+            // Add results to the corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+}
+
+
+function calcAverage(tips) {
+    var sum = 0;
+    for (var i = 0; i < tips.length; i++) {
+        sum = sum + tips[i];
+    }
+    return sum / tips.length;
+}
+
+// Do the calculations
+ibrahim.calcTips();
+khalil.calcTips();
+
+ibrahim.average = calcAverage(ibrahim.tips);
+khalil.average = calcAverage(khalil.tips);
+console.log(ibrahim, khalil);
+
+if (ibrahim.average > khalil.average) {
+    console.log(ibrahim.fullName + ' \'s family pays higher tips, with an average of $' + ibrahim.average);
+} else if (khalil.average > ibrahim.average) {
+    console.log(khalil.fullName + ' \'s family pays higher tips, with an average of $' + khalil.average);
+}
 
 
 
